001033b4a0598f3644fb155bdd9f7fb481477ba1
Srashti3103 <srashtipatel31@gmail.com>
2025-12-09 22:07:22 +0530

    FORMATED_INPUT_OUTPUT

---
+++ C++/IOSTREAM/FORMATED_INPUT_OUTPUT.cpp
@@ -0,0 +1,12 @@
+//FORMATED INPUT OUTPUT
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	cout.width(8);
+	cout.precision(2);
+	cout.fill('*');
+	cout<<12.16412<<endl;//printf("%*8.2f",12.16412);
+	cout<<15.1356<<endl;
+	cout<<12.20004;
+}
