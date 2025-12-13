eec121fecba9d4581e0a896a4488c39f7fa2a09e
Srashti3103 <srashtipatel31@gmail.com>
2025-12-13 13:56:01 +0530

    operator overloading civision

---
+++ C++/PRACTICE/OPERATOR_OVERLOADING_DIVISION.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class complex{
+	private : int a,b;
+	public: complex(){
+	}
+	complex(int x,int y){
+		a=x;
+		b=y;
+	}
+	void out(){
+		cout<<a<<"+i"<<b<<endl;
+	}
+	complex operator/(complex x){
+		complex w;
+		w.a=(a*x.a+b*x.b)/(x.a*x.a+x.b*x.b);
+		w.b=(b*x.a-a*x.b)/(x.a*x.a+x.b*x.b);
+		return(w);
+	}
+};
+int main(){
+	complex p(3,2);
+	complex q(2,4);
+	complex t;
+	t=p/q;
+	p.out();
+	q.out();
+	t.out();
+}
