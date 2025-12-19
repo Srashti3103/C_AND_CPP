e01125220c980d87b060b27500ffced64d53ba03
Srashti3103 <srashtipatel31@gmail.com>
2025-12-19 21:15:52 +0530

    PURE_VIRTUAL_FUNCTION

---
+++ C++/PURE_VIRTUAL_FUNCTION.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class graphics{
+	public: virtual void draw(){
+		cout<<"GRAPHIC DRAW CALLED "<<endl;
+	}
+	virtual void show()=0;//pure virtual function
+};
+class ract: public graphics{
+	public:void draw(){
+		cout<<"ract draw called "<<endl;
+	}
+	virtual void show(){
+		cout<<"ract show called";
+	}; 
+};
+class square:public graphics{
+	public:void show(){
+		cout<<"square show called "<<endl;
+	}
+};
+int main(){
+	graphics *p;
+	ract r;
+	p=&r;
+	p->draw();//ract draw called
+	square s;
+	p=&s;
+	p->draw();//graphic draw called
+	p->show();//square show called 
+}
