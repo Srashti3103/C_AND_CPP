29c1275669454f89ca6c1c29772155121f86631d
Srashti3103 <srashtipatel31@gmail.com>
2025-12-20 22:56:24 +0530

    NORMAL_OBJECT

---
+++ C++/NORMAL_OBJECT.cpp
@@ -0,0 +1,20 @@
+//POINTER TO OBJECT 
+//INPUT AND PRINT TWO NUMBERS
+#include<iostream>
+using namespace std;
+class test{
+	private: int a , b;
+	public: void get(){
+		cout<<"ENTER TWO NUMBER"<<endl;
+		cin>>a>>b;
+	}
+	void out(){
+		cout<<a<<endl<<b<<endl;
+	}
+};
+//NORMAL OBJECT
+int main(){
+	test t;
+	t.get();
+	t.out();
+}
