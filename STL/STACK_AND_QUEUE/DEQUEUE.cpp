702e37a1822575e1bfcab37fe4e2636efdb8dec7
Srashti3103 <srashtipatel31@gmail.com>
2026-01-14 23:05:38 +0530

    DEQUEUE.cpp

---
+++ STL/STACK_AND_QUEUE/DEQUEUE.cpp
@@ -0,0 +1,33 @@
+//DEQUEUE
+//DOUBLE ENDED QUEUE
+#include<iostream>
+#include<deque>
+using namespace std;
+void showdq(deque<int>g){
+	deque<int>::iterator it;
+	for(it = g.begin();it!=g.end();++it)
+	cout<<'\t'<<*it;
+	cout<<'\n';
+}
+int main(){
+	deque<int>dq;
+	dq.push_back(10);
+	dq.push_front(20);
+	dq.push_back(30);
+	dq.push_front(15);
+	cout<<"the deque dq is";
+	showdq(dq);
+	cout<<"\ndq.size(): "<<dq.size();
+	cout<<"\ndq.max_size():"<<dq.max_size();
+	cout<<"\ndq.at(2):"<<dq.at(2);
+	cout<<"\ndq.front():"<<dq.front();
+	cout<<"ndq.back():"<<dq.back();
+	cout<<"\ndq.pop_front():";
+	dq.pop_front();
+	showdq(dq);
+	cout<<"\ndq.pop_back():";
+	dq.pop_back();
+	showdq(dq);
+	return 0;
+	
+}
