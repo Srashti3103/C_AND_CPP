dbed9734f9ddfa125b6f0a4e5411724bf99d7c89
Srashti3103 <srashtipatel31@gmail.com>
2026-01-18 13:36:29 +0530

    identity matrix

--- PRACTICE/IDENTITY_MATRIX.cpp
+++ PRACTICE/IDENTITY_MATRIX.cpp
@@ -1,31 +1,26 @@
-//create identity matrix and print marix
+//create identity matrix and print matrix
 #include<stdio.h>
-int main()
-{
-	int a[10][10],m , n ;
-	printf("Enter row and coloumnn \n");
+#include<string.h>
+int main(){
+	char a[10][10];
+	int m,n;
+	printf("ENTER NUMBER OF ROW AND COLOUMN : \n");
 	scanf("%d%d",&m,&n);
-	for (int i =0 ; i<m ; i++)
-	{
-		for (int j=0 ; j<n; j++)
-		{
-			if(i==j)
-			{
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			if(i==j){
 				a[i][j]=1;
-			}
-				else
-			{
+			}else{
 				a[i][j]=0;
 			}
 		}
 	}
-		printf("IDENTITY MATRIX \n");
-		for (int i=0 ; i<m; i++)
-		{
-			for (int j=0 ; j<n; j++)
-			{
-				printf("%4d",a[i][j]);
-			}
-			printf("\n");
+	printf("IDENTITY MATRIXX :\n");
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			printf("%4d",a[i][j]);
 		}
+		printf("\n");
+	}
+	
 }
