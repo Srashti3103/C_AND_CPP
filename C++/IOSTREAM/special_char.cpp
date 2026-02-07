a4176b72faf15b66300d6e71e7d1f49c4e0daa8c
Srashti3103 <srashtipatel31@gmail.com>
2026-02-07 18:54:04 +0530

    adding two number using function overloading

--- C++/IOSTREAM/special_char.cpp
+++
@@ -1,9 +0,0 @@
-//INPUT AND PRINT SPECIAL CHARACTERS
-#include<iostream>
-using namespace std;
-int main(){
-	char ch[80];
-	cout<<"ENTER NAME = "<<endl;
-	cin.getline (ch,30);
-	cout.write(ch,30);  
-}
